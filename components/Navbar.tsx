b6a7b6ac458c8ee7a134eecdd5b7e3c731c1e40e
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 17:20:50 +0530

    Added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                Shortly
+            </div>
+            <div>
+                <p>Features</p>
+                <p>Pricing</p>
+                <p>Resources</p>
+            </div>
+        </div>
+        <div>
+            <p>Login</p>
+            <p>Signup</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

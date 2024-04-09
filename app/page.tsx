710d9500ca2b1d52cd311dff9aff80a8ae13037b
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 18:07:47 +0530

    Main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
@@ -7,6 +8,7 @@ const page = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <Main/>
     </div>
   )
 }

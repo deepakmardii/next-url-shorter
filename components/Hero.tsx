473fafc447551de2eaa1c6dcd3e1c0427cfbc80c
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 18:05:40 +0530

    Next Image added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 function Hero() {
     return (
         <div className='static md:ml-20 flex flex-col md:flex-row mb-[10rem]' >
             <div className=' h-[21.5rem] md:h-[28rem] order-1 relative md:order-2 overflow-hidden md:w-full md:items-center md:flex'>
                 <div className='absolute w-[28rem] md:w-[40rem] md:right-[-5rem] right-[-7.6rem]'>
-                    <img className='h-full w-full' src="/images/illustration-working.svg" alt="" />
+                    <Image className='h-full w-full' src="/images/illustration-working.svg" alt="illustration working" width={733} height={482}/>
                 </div>
             </div>
             <div className='mx-10 md:mx-0 order-2 md:order-1 flex flex-col items-center md:items-start text-center md:text-left gap-4 md:h-[28rem] md:w-[60rem] md:justify-center font-medium'>

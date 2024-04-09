440cd7fa93576b1768c7ef198a0a635fd0e3d436
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 18:20:17 +0530

    tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        "bluish":"#00d6cf"
-      }
+        'primary': 'var(--primary)',
+        'dark-violet': 'var(--dark-violet)',
+        'very-dark-blue': 'var(--very-dark-blue)',
+        'danger': 'var(--danger)',
+        'gray': 'var(--gray)',
+        'light-gray': 'var(--light-gray)',
+        'grayish-violet': 'var(--grayish-violet)',
+        'very-dark-violet': 'var(--very-dark-violet)',
+        'modal-bg': 'var(--modal-bg)',
+      },
+      backgroundImage: {
+        'bg-shorten-mobile': "url('/images/bg-shorten-mobile.svg')",
+        'bg-shorten-desktop': "url('/images/bg-shorten-desktop.svg')",
+        'bg-boost-mobile': "url('/images/bg-boost-mobile.svg')",
+        'bg-boost-desktop': "url('/images/bg-boost-desktop.svg')",
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}

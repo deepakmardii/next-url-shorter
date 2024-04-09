0908c54b4b9e531448bf81917e612b1c5e6cc1cf
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 18:27:35 +0530

    Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image'
-function Hero() {
-    return (
-        <div className='static md:ml-20 flex flex-col md:flex-row mb-[10rem]' >
-            <div className=' h-[21.5rem] md:h-[28rem] order-1 relative md:order-2 overflow-hidden md:w-full md:items-center md:flex'>
-                <div className='absolute w-[28rem] md:w-[40rem] md:right-[-5rem] right-[-7.6rem]'>
-                    <Image className='h-full w-full' src="/images/illustration-working.svg" alt="illustration working" width={733} height={482}/>
-                </div>
-            </div>
-            <div className='mx-10 md:mx-0 order-2 md:order-1 flex flex-col items-center md:items-start text-center md:text-left gap-4 md:h-[28rem] md:w-[60rem] md:justify-center font-medium'>
-                <div className='text-4xl font-semibold text-zinc-800 md:text-6xl md:font-bold md:leading-[4.5rem]'>More than just shorter links</div>
-                <div className='text-gray-400 md:text-xl'> Build your brand’s recognition and get detailed insights on how your links are performing.</div>
-                <button className='hover:opacity-50 cursor md:mt-4 text-md text-center bg-bluish px-10 py-3 text-white rounded-full font-semibold'>Get Started</button>
-            </div>
 
-        </div>
+const Hero = () => {
+    return (
+        <section className="hero overflow-x-hidden flex md:flex-row md:text-left text-center flex-col-reverse justify-between items-center lg:px-32 md:px-16 px-5 mt-8">
+            <div className="hero-left lg:w-1/2 md:w-2/3 w-full">
+                <h1
+                    className="hero-title text-very-dark-blue font-bold xl:text-7xl lg:text-6xl text-4xl"
+                    style={{ lineHeight: 1.2 }}
+                >
+                    More than just shorter links
+                </h1>
+                <p className="hero-description md:max-w-[90%] mt-1 lg:mb-10 mb-5 text-grayish-violet">
+                    Build your brand&apos;s recognition and get detailed insights on how your links are performing.
+                </p>
+            </div>
+            <div className="hero-right lg:w-1/2 md:w-11/12 w-full 2xl:static md:relative md:-right-32 mb-10">
+                <Image className='w-full' priority src="/images/illustration-working.svg" alt="working illustration" aria-hidden="true" role='presentation' width={500} height={500} />
+            </div>
+        </section>
     )
 }
 

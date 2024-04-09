85dfb518b1c590478e016cb79d179b6fdae5f73e
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-09 18:24:58 +0530

    Navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,78 +1,75 @@
-"use client";
-import { VscChromeClose } from "react-icons/vsc";
-import { AiOutlineMenu } from "react-icons/ai";
-import Link from "next/link";
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 
-import { useState } from "react";
-import Image from "next/image";
+const Navbar = () => {
 
-function Navbar() {
-  const [showMe, setShowMe] = useState(false);
-  function toggle() {
-    setShowMe(!showMe);
-  }
-  console.log(showMe);
-  const gap = "gap-10";
-  const linkHover = "  md:hover:text-Violet  ";
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  return (
-    <>
-      <nav
-        className={
-          "font-medium py-10 md:py-0 px-8 md:px-20 flex items-center justify-between md:justify-start max-w-7xl mx-auto text-lg  "
-        }
-      >
-        <div className="logo md:mr-10">
-          <Image src="/images/logo.svg" alt="logo" height={33} width={121} />
-        </div>
-        <div
-          className={`${
-            showMe ? "" : "hidden"
-          } md:flex absolute md:static md:items-center rounded-xl top-24 bg-Violet md:bg-transparent md:text-gray-400 text-white md:justify-between md:w-full flex-col md:flex-row inset-x-0 mx-8 z-50`}
-        >
-          <div
-            className={`links flex flex-col items-center gap-6 my-8 md:flex-row md:${gap}`}
-          >
+    return (
+        <header className='lg:px-32 md:px-16 py-10 px-5 flex relative justify-start items-center'>
             <Link href="/">
-              <p className={linkHover}>Feature </p>
+                <Image src="/images/logo.svg" width="121" height="33" alt='logo' />
             </Link>
-            <Link href="/">
-              <p className={linkHover}>Pricing</p>
-            </Link>
-            <Link href="/">
-              <p className={linkHover}>Resources</p>
-            </Link>
-          </div>
-          <hr className="border w-[80%] mx-8 border-gray-600 md:hidden" />
-          <div
-            className={`right flex w-full md:w-auto items-center flex-col md:flex-row gap-8 my-8 md:${gap}`}
-          >
-            <Link href="/">
-              <p className={linkHover}>Login</p>
-            </Link>
-            <Link href="/">
-              <p className="hover:opacity-50 cursor text-center bg-bluish px-6 py-2 text-white rounded-full ">
-                Sign Up
-              </p>
-            </Link>
-          </div>
-        </div>
-        <div className="menu md:hidden">
-          {showMe ? (
-            <VscChromeClose
-              className="fill-gray-400 text-4xl cursor-pointer"
-              onClick={toggle}
-            />
-          ) : (
-            <AiOutlineMenu
-              className="fill-gray-400 text-4xl cursor-pointer"
-              onClick={toggle}
-            />
-          )}
-        </div>
-      </nav>
-    </>
-  );
+
+            <button className='sm:hidden absolute right-5' onClick={() => setIsMenuOpen(prevState => !prevState)}>
+                <i className={`fas fa-${isMenuOpen ? 'times' : 'bars'} text-very-dark-violet text-2xl`}></i>
+            </button>
+
+            <nav className={`
+                ${isMenuOpen ? 'flex' : 'hidden'}
+                sm:flex sm:flex-row 
+                sm:text-left text-center
+                sm:w-fit w-[calc(100%-40px)]
+                flex-col fixed sm:static
+                top-20 items-center
+                justify-between
+                sm:bg-transparent bg-dark-violet
+                sm:p-0 p-5
+                rounded-lg
+                z-50
+            `}>
+
+                <ul className="sm:ml-10 flex sm:flex-row flex-col sm:gap-5 gap-3 font-semibold">
+                    <li>
+                        <Link href="/features">
+                            <p className="sm:text-grayish-violet text-white text-sm sm:hover:text-very-dark-violet">Features</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/pricing">
+                            <p className="sm:text-grayish-violet text-white text-sm sm:hover:text-very-dark-violet">Pricing</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/resources">
+                            <p className="sm:text-grayish-violet text-white text-sm sm:hover:text-very-dark-violet">Resources</p>
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="sm:hidden block divider h-[1px] w-full my-3 bg-grayish-violet" />
+
+                <ul className='flex sm:w-fit w-full items-center sm:flex-row flex-col sm:gap-5 gap-3 sm:absolute lg:right-32 md:right-16 right-5'>
+                    <li>
+                        <Link href="/login">
+                            <p className="sm:text-grayish-violet text-white text-sm sm:hover:text-very-dark-violet">Login</p>
+                        </Link>
+                    </li>
+                    <li className='w-full'>
+                        <Link href="/signup">
+                            <span
+                                className="bg-primary w-full hover:opacity-60 text-white text-sm px-4 py-2 rounded-full hover:bg-opacity-80">
+                                Sign Up
+                            </span>
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </header>
+    )
 }
 
-export default Navbar;
+export default Navbar
